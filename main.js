6c68c5b71f21f6268347a921a9206c348112cc8d
Artem Avdienko <avdart53@gmail.com>
2018-08-22 13:25:34 +0700

    Почти закнчил калькулятор

--- main.js
+++ main.js
@@ -13,61 +13,7 @@ const input = document.getElementById('num-input');
 num1.onclick = () => {
     enterNamber(1)
 };
-const num2 = document.getElementById('num2');
-num2.onclick = () => {
-    enterNamber(2)
-};
-const num3 = document.getElementById('num3');
-num3.onclick = () => {
-    enterNamber(3)
-};
-const num4 = document.getElementById('num4');
-num4.onclick = () => {
-    enterNamber(4)
-};
-const num5 = document.getElementById('num5');
-num5.onclick = () => {
-    enterNamber(5)
-};
-const num6 = document.getElementById('num6');
-num6.onclick = () => {
-    enterNamber(6)
-};
-const num7 = document.getElementById('num7');
-num7.onclick = () => {
-    enterNamber(7)
-};
-const num8 = document.getElementById('num8');
-num8.onclick = () => {
-    enterNamber(8)
-};
-const num9 = document.getElementById('num9');
-num9.onclick = () => {
-    enterNamber(9)
-};
-const num0 = document.getElementById('num0');
-num0.onclick = () => {
-    enterNamber(0)
-};
-const numg = document.getElementById('numg');
-numg.onclick = () => {
-    enterNamber('+')
-};
-const numn = document.getElementById('numn');
-numn.onclick = () => {
-    enterNamber('-')
-};
-const numh = document.getElementById('numh');
-numh.onclick = () => {
-    enterNamber('*')
-};const numj = document.getElementById('numj');
-numj.onclick = () => {
-    enterNamber('.')
-};
-const numk = document.getElementById('numk');
-numk.onclick = () => {
-    enterNamber('/')
-};
+
 enterNamber = (n) => {
     if (input.value === '0') {
         input.value = n;
